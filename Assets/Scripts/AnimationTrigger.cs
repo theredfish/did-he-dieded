af1e42d8940b4d51fb0f928e7c20e31201a1d178
Supamiu <fnormand@sodius.com>
2018-01-08 16:46:14 +0100

    WIP - Trigger for pikes

---
+++ Assets/Scripts/AnimationTrigger.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class AnimationTrigger : MonoBehaviour {
+
+	public Animator animator;
+
+	public string stateName;
+
+	public void OnTriggerEnter(Collider other){
+		if (other.CompareTag ("Player")) {
+			animator.Play(stateName);
+		}
+	}
+}
--- Assets/Scripts/DeathCollider.cs
+++ Assets/Scripts/DeathCollider.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Collider))]
 public class DeathCollider : MonoBehaviour {
 
 	void OnTriggerEnter(Collider other){
