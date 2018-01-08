9ee549773c370a1fb004c45c539dbf74f944f0db
supamiu <fnormand@sodius.com>
2018-01-08 16:02:07 +0100

    Initial Commit

---
+++ Assets/DeathCollider.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathCollider : MonoBehaviour {
+
+	void OnTriggerEnter(Collider other){
+		if (other.CompareTag ("Player")) {
+			other.gameObject.GetComponent<Player> ().Fall ();
+		}
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+[RequireComponent(typeof(Rigidbody))]
+public class Player : MonoBehaviour {
+
+	private float maxTpAmmo;
+
+	[Header("La puissance du saut")]
+	public float jumpForce = 5f;
+
+	[Header("Vitesse de déplacement horizontal")]
+	public float moveSpeed = 5f;
+
+	[Header("Portée du teleport")]
+	public float teleportRange = 1f;
+
+	public float tpAmmo = 2f;
+
+	private CharacterController controller;
+
+	public bool hasGravity = true;
+
+	// The current movement vector
+	private Vector3 movement = Vector3.zero;
+
+	// Use this for initialization
+	void Start () {
+		this.controller = GetComponent<CharacterController> ();
+		this.maxTpAmmo = this.tpAmmo;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// First of all, we get the base movement
+		movement.x = Input.GetAxis("Horizontal") * this.moveSpeed;
+		// Then we check for a TP.
+		if (Input.GetButtonDown("Teleport") && this.tpAmmo >= 1f) {
+			this.Teleport ();
+		}
+		// Jump
+		if (Input.GetButton("Jump") && this.controller.isGrounded) {
+			movement.y = this.jumpForce;
+		}
+		if (!this.controller.isGrounded && this.hasGravity) {
+			movement.y += Physics.gravity.y * Time.deltaTime;
+		}
+		this.controller.Move (movement * Time.deltaTime);
+	}
+
+	void Teleport() {
+		Vector3 direction = new Vector3 (Input.GetAxisRaw ("Horizontal"), Input.GetAxisRaw ("Vertical"), 0);
+		Vector3 tpVector = direction.normalized * teleportRange;
+		transform.position += tpVector;
+		this.tpAmmo--;
+	}
+
+	public void Fall() {
+		transform.position = Vector3.zero;
+		this.tpAmmo = maxTpAmmo;
+	}
+}
