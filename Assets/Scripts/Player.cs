d672ac626564fee17f147e6894a8cb2d1826fc84
theredfish <did.julian@gmail.com>
2018-01-19 16:34:14 +0100

    Add chibi model with animations

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,6 +6,9 @@ using UnityEngine;
 public class Player : MonoBehaviour {
 	private float _gravity = 1.0f;
 	private float maxTpAmmo;
+	private Animator anim;
+	private bool isForward = true;
+	private bool isBackward = false;
 
 	[Header("La puissance du saut")]
 	public float jumpForce = 5f;
@@ -18,6 +21,9 @@ public class Player : MonoBehaviour {
 
 	public float tpAmmo = 2f;
 
+	[Header("Le prefab de l'attaque")]
+	public Transform roseFireBall;
+
 	private CharacterController controller;
 
 	public bool hasGravity = true;
@@ -25,19 +31,28 @@ public class Player : MonoBehaviour {
 	// The current movement vector
 	private Vector3 movement = Vector3.zero;
 
+	private Transform StartAttackPosition;
+
 	// Use this for initialization
 	void Start () {
 		this.controller = GetComponent<CharacterController> ();
 		this.maxTpAmmo = this.tpAmmo;
+		this.anim = GetComponent<Animator>();
+		this.StartAttackPosition =  transform.Find("StartAttackPosition").transform;
 	}
 
-	void FixedUpdate () {
+	void Update () {
 		// First of all, we get the base movement
 		movement.x = Input.GetAxis("Horizontal") * this.moveSpeed;
 
 		// Then we check for a TP.
 		if (Input.GetButtonDown("Teleport") && this.tpAmmo >= 1f) {
 			this.Teleport ();
+		}
+
+		// We check any attack
+		if (Input.GetButtonDown("Fire1")) {
+			this.Shoot();
 		}
 
 		// Then we get the vertical movement
@@ -53,11 +68,36 @@ public class Player : MonoBehaviour {
 			movement.y -= _gravity;
 		}
 
-		this.controller.Move (movement * Time.deltaTime);
+		WalkOrIdle (movement);
+	}
+
+	void WalkOrIdle(Vector3 movement) {
+		this.controller.Move(movement * Time.deltaTime);
+		float axis = Input.GetAxisRaw("Horizontal");
+
+		if (axis < 0) {
+			if (isForward) {
+				transform.Rotate(new Vector3(0, -180, 0));
+				isForward = false;
+				isBackward = true;
+			}
+
+			anim.SetTrigger("isWalking");
+		} else if (axis > 0) {
+			if (isBackward) {
+				transform.Rotate (new Vector3 (0, 180, 0));
+				isForward = true;
+				isBackward = false;
+			}
+
+			anim.SetTrigger("isWalking");
+		} else {
+			anim.SetTrigger("isIdle");
+		}
 	}
 
 	void Teleport() {
-		Vector3 direction = new Vector3 (Input.GetAxisRaw ("Horizontal"), Input.GetAxisRaw ("Vertical"), 0);
+		Vector3 direction = new Vector3 (Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0);
 		Vector3 tpVector = direction.normalized * teleportRange;
 
 		transform.position += tpVector;
@@ -65,11 +105,20 @@ public class Player : MonoBehaviour {
 	}
 
 	void Shoot() {
-		
+		anim.SetTrigger("isAttacking");
+		Instantiate (roseFireBall, StartAttackPosition);
 	}
 
 	public void Fall() {
 		transform.position = Vector3.zero;
 		this.tpAmmo = maxTpAmmo;
+	}
+
+	public bool GetIsForward() {
+		return isForward;
+	}
+
+	public bool GetIsBackward() {
+		return isBackward;
 	}
 }
