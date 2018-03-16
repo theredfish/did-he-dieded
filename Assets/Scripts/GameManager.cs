b2a457bbde7b7766de7dd08b02e6d084f7b4b3be
MaximeLeGac <maxime.le.gac.44@gmail.com>
2018-03-16 15:03:06 +0100

    Add monsters on scene

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -12,10 +12,16 @@ public class GameManager : MonoBehaviour {
     private Player player;
     private StoneMonster stoneMonster;
     private SpikeMonster spikeMonster;
+    private SpikeMonster spikeMonster2;
+    private SpikeMonster spikeMonster3;
+    private SpikeMonster spikeMonster4;
 
     private Vector3 playerSpawn;
     private Vector3 stoneMonsterSpawn;
     private Vector3 spikeMonsterSpawn;
+    private Vector3 spikeMonsterSpawn2;
+    private Vector3 spikeMonsterSpawn3;
+    private Vector3 spikeMonsterSpawn4;
 
     private bool gameIsPaused = false;
     private StoppableGameobject[] stoppableGameobjects;
@@ -46,11 +52,23 @@ public class GameManager : MonoBehaviour {
         stoneMonsterSpawn = transform.Find("StoneMonsterSpawn").position;
         stoneMonster = GameObject.FindGameObjectWithTag("Monster").GetComponent<StoneMonster>();
         stoneMonster.transform.position = stoneMonsterSpawn;
-        /*
-        spikeMonsterSpawn = transform.Find("StoneMonsterSpawn").position;
+        
+        spikeMonsterSpawn = transform.Find("StoneMonsterSpawn1").position;
         spikeMonster = GameObject.FindGameObjectWithTag("SpikeMonster").GetComponent<SpikeMonster>();
         spikeMonster.transform.position = spikeMonsterSpawn;
-        */
+
+        spikeMonsterSpawn2 = transform.Find("StoneMonsterSpawn2").position;
+        spikeMonster2 = GameObject.FindGameObjectWithTag("SpikeMonster2").GetComponent<SpikeMonster>();
+        spikeMonster2.transform.position = spikeMonsterSpawn2;
+
+        spikeMonsterSpawn3 = transform.Find("StoneMonsterSpawn3").position;
+        spikeMonster3 = GameObject.FindGameObjectWithTag("SpikeMonster3").GetComponent<SpikeMonster>();
+        spikeMonster3.transform.position = spikeMonsterSpawn3;
+
+        spikeMonsterSpawn4 = transform.Find("StoneMonsterSpawn4").position;
+        spikeMonster4 = GameObject.FindGameObjectWithTag("SpikeMonster4").GetComponent<SpikeMonster>();
+        spikeMonster4.transform.position = spikeMonsterSpawn4;
+
 
         //eventSystem = GameObject.Find("EventSystem").GetComponent<EventSystem>();
 
